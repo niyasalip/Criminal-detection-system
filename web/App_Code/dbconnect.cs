37d0d12bab430a8ecab6c19040394df8f501e259
NIYAS ALI P <44407205+niyasalip@users.noreply.github.com>
2021-03-07 16:42:19 +0530

    Add files via upload

---
+++ web/App_Code/dbconnect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class dbconnect
+{
+    SqlConnection Con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\NIYAS\Documents\Visual Studio 2010\WebSites\criminal_detection1\App_Data\Criminal_detection.mdf");
+
+
+    public void Execute(SqlCommand cmd)//inser...dete...up..
+    {
+        cmd.Connection = Con;
+        Con.Open();
+        cmd.ExecuteNonQuery();
+        Con.Close();
+    }
+    public DataTable get_Data(SqlCommand cmd)//sel
+    {
+
+        cmd.Connection = Con;
+        SqlDataAdapter da = new SqlDataAdapter();
+        da.SelectCommand = cmd;
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        return ds.Tables[0];
+
+    }
+}
